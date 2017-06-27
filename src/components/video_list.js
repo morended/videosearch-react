2b7598f97ddeffade11983ab696a206d1f12199c
Mounika <mounika.rendedla@gmail.com>
2017-06-27 12:44:48 -0600

    Update video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,24 +2,19 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import VideoListItem from './video_list_item';
 
-
-
 class VideoList extends Component{
-
     constructor(props){
         super(props);
     }
-
 render(){
-//var  self=this;
     const  videoItems = this.props.videos.map((video) =>
         <VideoListItem key={video.etag}  video={video}  selectedChange={(selectedVideo) => this.props.selectedVideo(selectedVideo)}/>)
     return(
-        <ul className="col-md-4 list-group">
+            <ul className="col-md-4 list-group">
             {videoItems}
-        </ul>
-        );
-    }
+            </ul>
+          );
+        }
 }
 
 export default VideoList;
