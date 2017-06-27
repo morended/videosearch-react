a237b9da3a4bd9d4f6e7bd993b476b43f446140e
Mounika <mounika.rendedla@gmail.com>
2017-06-27 12:43:03 -0600

    Update video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -8,18 +8,12 @@ class VideoList extends Component{
 
     constructor(props){
         super(props);
-
     }
-
-
 
 render(){
 //var  self=this;
     const  videoItems = this.props.videos.map((video) =>
-
-            <VideoListItem key={video.etag}  video={video}  selectedChange={(selectedVideo) => this.props.selectedVideo(selectedVideo)}/>
-
-    )
+        <VideoListItem key={video.etag}  video={video}  selectedChange={(selectedVideo) => this.props.selectedVideo(selectedVideo)}/>)
     return(
         <ul className="col-md-4 list-group">
             {videoItems}
